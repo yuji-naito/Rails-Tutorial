ad75ba2fd8f8c0d6208c2262ff8c63a26c504a84
Naitya <naitya@example.com>
2018-02-26 11:31:49 +0000

    add StaticPages again

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   get 'statis_pages/home'
-
-  get 'statis_pages/help'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class StatisPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get statis_pages_home_url
+    get static_pages_home_url
     assert_response :success
   end
 
   test "should get help" do
-    get statis_pages_help_url
+    get static_pages_help_url
     assert_response :success
   end
 

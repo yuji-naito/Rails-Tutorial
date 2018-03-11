16e601e210512630ee0fe13b1b5dc6b234a602bd
Naitya <naitya@example.com>
2018-03-11 14:38:55 +0000

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,12 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '') # remember_tokenを参照する前のnew()でエラーになるため、引数は何でも良い
   end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end

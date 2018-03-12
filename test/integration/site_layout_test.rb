3dd0f6bd20424466d21a8d0830375ad73fbac784
Naitya <naitya@example.com>
2018-03-12 15:48:04 +0000

    Add user following

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -27,5 +27,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", users_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end

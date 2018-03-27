5d5782c06757de50f0d315d4fde9bcebb7844ccd
Naitya <naitya@example.com>
2018-03-27 15:30:16 +0000

    Add random logo images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+  
+  # ロゴイメージのファイル名をランダムで1つ返す
+  def random_image
+    # ランダムに表示するロゴのファイル名を配列で宣言
+    logo_images = ["MyMeat.png", "MyCake.png"]
+    
+    # ロゴの配列からランダムで1つ文字列を代入
+    logo_image = logo_images[rand(logo_images.length)]
+    
+    return logo_image
+  end
 end
